c695d74ee8f213f8dd65a88a5bf40599c7fdcf23
Markus Hitter <mah@jump-ing.de>
2018-05-15 14:33:48 +0200

    Standardize module name.
    This is, all uppercase and 'Block' to the front.

--- blockcms.php
+++ blockcms.php
@@ -46,7 +46,7 @@ class BlockCms extends Module
 		$this->bootstrap = true;
 		parent::__construct();
 
-		$this->displayName = $this->l('CMS block');
+		$this->displayName = $this->l('Block CMS');
 		$this->description = $this->l('Adds a block with several CMS links.');
 		$this->secure_key = Tools::encrypt($this->name);
 		$this->ps_versions_compliancy = array('min' => '1.6', 'max' => '1.6.99.99');
